81dbff42e02831a09da5808bdbfdc52599a79dc8
Daria Goremykina <d.goremykina@gmail.com>
2022-06-07 01:45:23 +0200

    1

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dgoremyk <dgoremyk@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/30 22:44:10 by dgoremyk          #+#    #+#             */
+/*   Updated: 2022/05/30 23:11:09 by dgoremyk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include "get_next_line_utils.c"
+
+char *get_next_line(int fd);
+
+#endif
