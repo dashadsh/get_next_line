9bb7c58d7fe8da1ac5dd5e840da61923e624535d
Daria Goremykina <d.goremykina@gmail.com>
2022-06-21 16:32:42 +0200

    1

--- main.c
+++ main.c
@@ -1,13 +1,16 @@
  #include <stdio.h>
- 
+ #include <unistd.h>
+ #include <fcntl.h>
+ #include <stdlib.h>
+
  int main()
 {
-	//FILE *fd = fopen("text.txt", "r");
-	fd = open("text.txt", O_RDONLY); 
+	int fd = open("text.txt", O_RDONLY); 
 	//create a buffer and give it a size of 100
-	char *c = (char *) calloc(100, sizeof(char));
-
-	read("text.txt", )
-	fclose(fd);
+	char *b = (char *) calloc(100000, sizeof(char));
+	//static char b[100];
+	read(fd, b, 12);
+	printf("%s\n", b);
+	close(fd);
 	return 0;
 }
