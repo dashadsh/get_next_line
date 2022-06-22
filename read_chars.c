706f41932231031de634a1fc6eb411a9cf7d0675
Daria Goremykina <d.goremykina@gmail.com>
2022-06-22 13:42:00 +0200

    1

---
+++ read_chars.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+/*
+int main()
+{
+//creates a "new" file if not exists
+	int fd = open("new", O_RDWR | O_CREAT);
+	close(fd);
+	return 0;
+}
+*/
+
+int main()
+{
+	int fd = open("text.txt", O_RDONLY);
+	//create a buffer and give it a size of 100
+	char *b = (char *) calloc(1, sizeof(char));
+	//char b[100];
+	read(fd, b, 12);
+	printf("%s\n", b);
+	close(fd);
+	return 0;
+}
